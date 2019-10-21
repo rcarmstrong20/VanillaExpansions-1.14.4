b0687565ad53b3112977b022995928da7c369a46
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-10-21 10:12:31 -0600

    Re-named the ONION_AGE property to AGE and Fixed the bounding boxes for the three age crops

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VeThreeStageCropBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VeThreeStageCropBlock.java
@@ -1,15 +1,23 @@
 package rcarmstrong20.vanilla_expansions.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.item.Items;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.IItemProvider;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 import rcarmstrong20.vanilla_expansions.init.VeBlocks;
 import rcarmstrong20.vanilla_expansions.init.VeItems;
 
 public class VeThreeStageCropBlock extends VeSevenStageCropBlock
 {
-	public static final IntegerProperty ONION_AGE = BlockStateProperties.AGE_0_3;
+	public static final IntegerProperty AGE = BlockStateProperties.AGE_0_3;
+	private static final VoxelShape[] ONION_SHAPE_BY_AGE = new VoxelShape[]{Block.makeCuboidShape(4.0D, 0.0D, 4.0D, 12.0D, 8.0D, 12.0D), Block.makeCuboidShape(3.0D, 0.0D, 3.0D, 13.0D, 11.0D, 13.0D), Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 14.0D, 14.0D), Block.makeCuboidShape(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D)};
+	private static final VoxelShape[] GINGER_SHAPE_BY_AGE = new VoxelShape[]{Block.makeCuboidShape(2.0D, 0.0D, 2.0D, 14.0D, 6.0D, 14.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 11.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 14.0D, 16.0D), Block.makeCuboidShape(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D)};
 	
 	public VeThreeStageCropBlock(Properties properties)
 	{
@@ -19,7 +27,16 @@ public class VeThreeStageCropBlock extends VeSevenStageCropBlock
 	@Override
 	public IntegerProperty getAgeProperty()
 	{
-		return ONION_AGE;
+		return AGE;
+	}
+	
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
+	{
+		if(this == VeBlocks.green_onions || this == VeBlocks.garlic)
+		{
+			return ONION_SHAPE_BY_AGE[state.get(this.getAgeProperty())];
+		}
+		return GINGER_SHAPE_BY_AGE[state.get(this.getAgeProperty())];
 	}
 	
 	@Override
