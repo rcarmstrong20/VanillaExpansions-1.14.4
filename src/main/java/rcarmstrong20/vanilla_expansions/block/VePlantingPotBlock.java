3ca5e857a69530a7cadc670664adbb90e6b4f076
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-10-24 22:07:13 -0600

    Added waterlogging capabilities to the planting pots

--- src/main/java/rcarmstrong20/vanilla_expansions/block/VePlantingPotBlock.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/block/VePlantingPotBlock.java
@@ -2,14 +2,25 @@ package rcarmstrong20.vanilla_expansions.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.fluid.IFluidState;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer.Builder;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
-public class VePlantingPotBlock extends VeCutoutBlock
+public class VePlantingPotBlock extends VeCutoutBlock implements IWaterLoggable
 {
+	public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+	
 	private static final VoxelShape PILLAR_1_AABB = Block.makeCuboidShape(4.0, 1.0, 4.0, 6.0, 8.0, 6.0);
 	private static final VoxelShape PILLAR_2_AABB = Block.makeCuboidShape(12.0, 1.0, 12.0, 10.0, 8.0, 10.0);
 	private static final VoxelShape PILLAR_3_AABB = Block.makeCuboidShape(6.0, 1.0, 12.0, 4.0, 8.0, 10.0);
@@ -42,5 +53,33 @@ public class VePlantingPotBlock extends VeCutoutBlock
 	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context)
 	{
 		return PLANTING_POT_AABB;
+	}
+	
+	@Override
+	public BlockState getStateForPlacement(BlockItemUseContext context)
+	{
+		return this.getDefaultState().with(WATERLOGGED, false);
+	}
+	
+	@Override
+	public BlockState updatePostPlacement(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos)
+	{
+		if (stateIn.get(WATERLOGGED))
+		{
+			worldIn.getPendingFluidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickRate(worldIn));
+		}
+		return stateIn;
+	}
+	
+	@Override
+	public IFluidState getFluidState(BlockState state)
+	{
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStillFluidState(false) : Fluids.EMPTY.getDefaultState();
+	}
+	
+	@Override
+	protected void fillStateContainer(Builder<Block, BlockState> builder)
+	{
+		builder.add(WATERLOGGED);
 	}
 }
