87160f371b9b976888c313d6741cef077cdcbe8f
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-12 09:28:24 -0700

    Optimized registry

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
@@ -3,25 +3,38 @@ package rcarmstrong20.vanilla_expansions.core;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.Blocks;
 import net.minecraft.tileentity.CampfireTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
+/**
+ * Author: rcarmstrong20
+ */
+@Mod.EventBusSubscriber(modid = VanillaExpansions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VeTileEntityType
 {
 	@SuppressWarnings("rawtypes")
 	public static final List<TileEntityType> TILE_ENTITY_TYPES = new ArrayList<>();
 	
-	public static TileEntityType<?> campfire = buildType(VanillaExpansions.vanillaLocation("campfire"), TileEntityType.Builder.create(CampfireTileEntity::new, Blocks.CAMPFIRE, VeBlocks.white_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
+	public static TileEntityType<CampfireTileEntity> campfire = buildType(VanillaExpansions.vanillaLocation("campfire"), TileEntityType.Builder.create(CampfireTileEntity::new, VeBlocks.white_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
 	
-	private static <T extends TileEntity> TileEntityType<T> buildType(ResourceLocation id, TileEntityType.Builder<T> builder)
+	private static <T extends TileEntity> TileEntityType<T> buildType(ResourceLocation name, TileEntityType.Builder<T> builder)
     {
         TileEntityType<T> type = builder.build(null);
-        type.setRegistryName(id);
+        type.setRegistryName(name);
         TILE_ENTITY_TYPES.add(type);
         return type;
+    }
+	
+	@SubscribeEvent
+    public static void registerTypes(final RegistryEvent.Register<TileEntityType<?>> event)
+    {
+        TILE_ENTITY_TYPES.forEach(type -> event.getRegistry().register(type));
+        TILE_ENTITY_TYPES.clear();
     }
 }
