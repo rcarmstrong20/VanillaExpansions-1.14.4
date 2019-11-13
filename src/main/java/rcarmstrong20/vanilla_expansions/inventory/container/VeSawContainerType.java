b49640e6ac45140b5a92bc4e49a0a5ec67ffc093
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-13 09:48:01 -0700

    Optimized container type registry

--- src/main/java/rcarmstrong20/vanilla_expansions/inventory/container/VeSawContainerType.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/inventory/container/VeSawContainerType.java
@@ -1,21 +1,32 @@
 package rcarmstrong20.vanilla_expansions.inventory.container;
 
-import net.minecraft.inventory.container.Container;
+import java.util.ArrayList;
+import java.util.List;
+
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
-public class VeSawContainerType extends ContainerType<Container>
+public class VeSawContainerType
 {
-	public static final ContainerType<?> SAW = registerContainerType(VeSawContainer::new).setRegistryName(VanillaExpansions.location("saw"));
+	public static final List<ContainerType<?>> CONTAINER_TYPES = new ArrayList<>();
 	
-	@SuppressWarnings("unchecked")
-	public VeSawContainerType(IFactory<?> factory)
+	public static final ContainerType<?> SAW = buildType(VanillaExpansions.location("saw"), VeSawContainer::new);
+	
+	public static ContainerType<?> buildType(ResourceLocation name, ContainerType.IFactory<?> factory)
 	{
-		super((IFactory<Container>) factory);
+		ContainerType<?> type = new ContainerType<>(factory);
+		type.setRegistryName(name);
+		CONTAINER_TYPES.add(type);
+		return type;
 	}
 	
-	public static ContainerType<?> registerContainerType(ContainerType.IFactory<?> factory)
-	{
-		return new ContainerType<>(factory);
-	}
+	@SubscribeEvent
+    public static void registerTypes(final RegistryEvent.Register<ContainerType<?>> event)
+    {
+		CONTAINER_TYPES.forEach(type -> event.getRegistry().register(type));
+		CONTAINER_TYPES.clear();
+    }
 }
