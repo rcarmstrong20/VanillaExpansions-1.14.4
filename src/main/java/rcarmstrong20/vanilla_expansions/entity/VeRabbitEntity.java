3933cf2956e69a6d8c66c8c54b1f4a582df6a75a
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-14 22:55:42 -0700

    Added the rabbit entity class

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/entity/VeRabbitEntity.java
@@ -0,0 +1,49 @@
+package rcarmstrong20.vanilla_expansions.entity;
+
+import java.util.UUID;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.effect.LightningBoltEntity;
+import net.minecraft.entity.passive.RabbitEntity;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.Util;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+import rcarmstrong20.vanilla_expansions.VanillaExpansions;
+
+public class VeRabbitEntity extends RabbitEntity
+{
+	/** Stores the UUID of the most recent lightning bolt to strike */
+	private UUID lightningUUID;
+	
+	public VeRabbitEntity(EntityType<? extends RabbitEntity> p_i50247_1_, World p_i50247_2_)
+	{
+		super(p_i50247_1_, p_i50247_2_);
+	}
+	CowEntity
+	/**
+	 * Called when a lightning bolt hits the entity.
+	 */
+	public void onStruckByLightning(LightningBoltEntity lightningBolt)
+	{
+		UUID uuid = lightningBolt.getUniqueID();
+		if (!uuid.equals(this.lightningUUID))
+		{
+			if(this.getRabbitType() == 1 || this.getRabbitType() == 99)
+			{
+				this.setRabbitType(this.getRabbitType() == 1 ? 99 : 1);
+				this.setCustomName(this.getCustomName() == TranslationTextComponent(Util.makeTranslationKey("entity", VanillaExpansions.vanillaLocation("killer_bunny"))) ? null : this.getCustomName());
+				
+				this.lightningUUID = uuid;
+				this.playSound(SoundEvents.ENTITY_MOOSHROOM_CONVERT, 2.0F, 1.0F);
+				/*
+				if(this.getCustomName() != null && this.getRabbitType() == 1)
+				{
+					this.setCustomName(null);
+				}
+				*/
+			}
+		}
+	}
+}
