36ddc19e0ff42754fffb6eaa392dff8f7d8394b9
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-22 16:56:41 -0700

    Finished the glass vial

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
@@ -1,10 +1,12 @@
 package rcarmstrong20.vanilla_expansions.item;
 
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.GlassBottleItem;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.SoundEvents;
 import rcarmstrong20.vanilla_expansions.core.VeItems;
@@ -19,16 +21,22 @@ public class VeGlassVialItem extends GlassBottleItem
 	@Override
 	public boolean itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, Hand hand)
 	{
-		if(stack.getItem() == VeItems.glass_vial)
+		if(stack.getItem() == VeItems.glass_vial && target instanceof MobEntity & target.getType() != EntityType.ENDER_DRAGON && target.getType() != EntityType.WITHER)
 		{
-			if (target instanceof MobEntity && ((MobEntity)target).getMaxHealth() >= ((MobEntity)target).getMaxHealth() / 4.0F)
+			//String entity_name = target.getType().getName().getString().toLowerCase();
+			
+			((MobEntity)target).playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
+			((MobEntity)target).attackEntityFrom(DamageSource.ON_FIRE, 1);
+			playerIn.addItemStackToInventory(new ItemStack(VeItems.blood_vial));
+			stack.shrink(1);
+			/*
+			if(target.getHealth() <= 0.0F)
 			{
-				((MobEntity)target).playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
-				((MobEntity)target).setHealth(target.getHealth() - 2);
-				playerIn.addItemStackToInventory(new ItemStack(VeItems.blood_vial));
-				stack.shrink(1);
-				return true;
+				target.playSound(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity." + entity_name + ".death")), 1.0F, 1.0F);
+				target.onDeath(DamageSource.MAGIC);
 			}
+			*/
+			return true;
 		}
 		return false;
 	}
