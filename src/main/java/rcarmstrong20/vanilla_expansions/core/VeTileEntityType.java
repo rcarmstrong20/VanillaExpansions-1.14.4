a66893a55c288d786ad27e3c8fc0ef2f0a74136b
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-22 16:56:23 -0700

    Unregistered the camp fire tile entity

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
@@ -3,6 +3,7 @@ package rcarmstrong20.vanilla_expansions.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.block.Blocks;
 import net.minecraft.tileentity.CampfireTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
@@ -20,8 +21,9 @@ public class VeTileEntityType
 {
 	public static final List<TileEntityType<?>> TILE_ENTITY_TYPES = new ArrayList<>();
 	
-	public static TileEntityType<CampfireTileEntity> campfire = buildType(VanillaExpansions.vanillaLocation("campfire"), TileEntityType.Builder.create(CampfireTileEntity::new, VeBlocks.white_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
+	//public static TileEntityType<CampfireTileEntity> campfire = buildType(VanillaExpansions.vanillaLocation("campfire"), TileEntityType.Builder.create(CampfireTileEntity::new, Blocks.CAMPFIRE, VeBlocks.white_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
 	
+	@SuppressWarnings("unused")
 	private static <T extends TileEntity> TileEntityType<T> buildType(ResourceLocation name, TileEntityType.Builder<T> builder)
     {
         TileEntityType<T> type = builder.build(null);
