fb4b40ead739a0a243dcb20599dbbb3da9b4528b
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-23 12:11:55 -0700

    Fixed the damage portion of the code

--- src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/item/VeGlassVialItem.java
@@ -23,19 +23,10 @@ public class VeGlassVialItem extends GlassBottleItem
 	{
 		if(stack.getItem() == VeItems.glass_vial && target instanceof MobEntity & target.getType() != EntityType.ENDER_DRAGON && target.getType() != EntityType.WITHER)
 		{
-			//String entity_name = target.getType().getName().getString().toLowerCase();
-			
-			((MobEntity)target).playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
-			((MobEntity)target).attackEntityFrom(DamageSource.ON_FIRE, 1);
+			((MobEntity) target).playSound(SoundEvents.ITEM_BOTTLE_FILL, 1.0F, 1.0F);
+			((MobEntity) target).attackEntityFrom(DamageSource.GENERIC, 1);
 			playerIn.addItemStackToInventory(new ItemStack(VeItems.blood_vial));
 			stack.shrink(1);
-			/*
-			if(target.getHealth() <= 0.0F)
-			{
-				target.playSound(ForgeRegistries.SOUND_EVENTS.getValue(new ResourceLocation("entity." + entity_name + ".death")), 1.0F, 1.0F);
-				target.onDeath(DamageSource.MAGIC);
-			}
-			*/
 			return true;
 		}
 		return false;
