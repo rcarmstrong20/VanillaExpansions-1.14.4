2b4104fbd7fb5653c22e62dc111c685e65a2804b
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-25 12:12:50 -0700

    Created VeRecipeSerializers to register the recipe serializers

---
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeRecipeSerializers.java
@@ -0,0 +1,39 @@
+package rcarmstrong20.vanilla_expansions.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.item.crafting.CampfireCookingRecipe;
+import net.minecraft.item.crafting.CookingRecipeSerializer;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.item.crafting.SingleItemRecipe;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import rcarmstrong20.vanilla_expansions.VanillaExpansions;
+import rcarmstrong20.vanilla_expansions.item.crafting.WoodCuttingRecipe;
+
+//@Mod.EventBusSubscriber(modid = VanillaExpansions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class VeRecipeSerializers
+{
+    private static final List<IRecipeSerializer<?>> RECIPES = new ArrayList<>();
+
+    public static final IRecipeSerializer<WoodCuttingRecipe> WOOD_CUTTING = register(VanillaExpansions.location("wood_cutting"), new SingleItemRecipe.Serializer<>(WoodCuttingRecipe::new));
+    
+	private static <T extends IRecipeSerializer<? extends IRecipe<?>>> T register(ResourceLocation name, T recipe)
+    {
+        recipe.setRegistryName(name);
+        RECIPES.add(recipe);
+        return recipe;
+    }
+    
+    @SubscribeEvent
+    public static void registerItems(final RegistryEvent.Register<IRecipeSerializer<?>> event)
+    {
+        RECIPES.forEach(item -> event.getRegistry().register(item));
+        RECIPES.clear();
+        
+        VanillaExpansions.LOGGER.info("Recipe serializers registered.");
+    }
+}
