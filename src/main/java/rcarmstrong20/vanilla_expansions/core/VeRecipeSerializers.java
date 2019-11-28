1829dc09a53e1f2fb3f6570a85af0b0fea674e6a
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-27 21:49:59 -0700

    Disabled the recipe serializer for the wood cutting recipe

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeRecipeSerializers.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeRecipeSerializers.java
@@ -3,24 +3,21 @@ package rcarmstrong20.vanilla_expansions.core;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.item.crafting.CampfireCookingRecipe;
-import net.minecraft.item.crafting.CookingRecipeSerializer;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.IRecipeSerializer;
-import net.minecraft.item.crafting.SingleItemRecipe;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
-import rcarmstrong20.vanilla_expansions.item.crafting.WoodCuttingRecipe;
 
 //@Mod.EventBusSubscriber(modid = VanillaExpansions.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class VeRecipeSerializers
 {
     private static final List<IRecipeSerializer<?>> RECIPES = new ArrayList<>();
 
-    public static final IRecipeSerializer<WoodCuttingRecipe> WOOD_CUTTING = register(VanillaExpansions.location("wood_cutting"), new SingleItemRecipe.Serializer<>(WoodCuttingRecipe::new));
+    //public static final IRecipeSerializer<WoodCuttingRecipe> WOOD_CUTTING = register(VanillaExpansions.location("wood_cutting"), new SingleItemRecipe.Serializer<>(WoodCuttingRecipe::new));
     
+	@SuppressWarnings("unused")
 	private static <T extends IRecipeSerializer<? extends IRecipe<?>>> T register(ResourceLocation name, T recipe)
     {
         recipe.setRegistryName(name);
