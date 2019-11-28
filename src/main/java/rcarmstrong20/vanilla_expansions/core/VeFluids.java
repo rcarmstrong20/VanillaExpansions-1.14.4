507ecee2cb166c697aca5c2265bebdad874e32b3
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-27 21:48:40 -0700

    Added the fog effects for the void liquid

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeFluids.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeFluids.java
@@ -3,9 +3,16 @@ package rcarmstrong20.vanilla_expansions.core;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
+import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.IFluidState;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -44,5 +51,34 @@ public class VeFluids
 	public static void onStitch(TextureStitchEvent.Pre event)
 	{
 		VanillaExpansions.LOGGER.info("Fluid textures registered.");
+	}
+	
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public void onFogColor(EntityViewRenderEvent.FogColors event)
+	{
+		ActiveRenderInfo info = event.getInfo();
+		IFluidState state = info.getFluidState();
+		if (state.getFluid() instanceof VoidFluid)
+		{
+			event.setRed(0.0F);
+			event.setGreen(0.0F);
+			event.setBlue(0.0F);
+		} 
+	}
+	
+	
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public void onFogDensity(EntityViewRenderEvent.FogDensity event)
+	{
+		ActiveRenderInfo info = event.getInfo();
+		IFluidState state = info.getFluidState();
+		if (state.getFluid() instanceof VoidFluid)
+		{
+			GlStateManager.fogMode(GlStateManager.FogMode.EXP);
+			event.setDensity(1.0F);
+			event.setCanceled(true);
+		} 
 	}
 }
