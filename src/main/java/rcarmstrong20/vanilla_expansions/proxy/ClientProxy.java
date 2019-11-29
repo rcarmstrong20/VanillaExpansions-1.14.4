1bf1f5401bbd3269c1d47e4ace2cdb40d5e32e12
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-28 23:44:52 -0700

    Added a method to register all of my particle factories

--- src/main/java/rcarmstrong20/vanilla_expansions/proxy/ClientProxy.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/proxy/ClientProxy.java
@@ -1,6 +1,9 @@
 package rcarmstrong20.vanilla_expansions.proxy;
 
+import net.minecraft.client.Minecraft;
 import rcarmstrong20.vanilla_expansions.client.renderer.VeBlockAndItemColors;
+import rcarmstrong20.vanilla_expansions.client.renderer.particle.VeVoidDripParticle;
+import rcarmstrong20.vanilla_expansions.core.VeParticleTypes;
 
 public class ClientProxy extends CommonProxy
 {
@@ -9,5 +12,13 @@ public class ClientProxy extends CommonProxy
 	{
 		super.onSetupClient();
 		VeBlockAndItemColors.registerColorHandlers();
+		this.registerParticleFactories();
+	}
+	
+	private void registerParticleFactories()
+	{
+		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.DRIPPING_VOID, VeVoidDripParticle.DrippingVoidFactory::new);
+		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.FALLING_VOID, VeVoidDripParticle.FallingVoidFactory::new);
+		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.LANDING_VOID, VeVoidDripParticle.LandingVoidFactory::new);
 	}
 }
