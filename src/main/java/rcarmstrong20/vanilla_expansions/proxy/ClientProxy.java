15e1cf6b2c86866a5c810144042753c53a2d05a0
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-11-30 12:01:37 -0700

    Added the undervoid particle factory renderer

--- src/main/java/rcarmstrong20/vanilla_expansions/proxy/ClientProxy.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/proxy/ClientProxy.java
@@ -2,6 +2,7 @@ package rcarmstrong20.vanilla_expansions.proxy;
 
 import net.minecraft.client.Minecraft;
 import rcarmstrong20.vanilla_expansions.client.renderer.VeBlockAndItemColors;
+import rcarmstrong20.vanilla_expansions.client.renderer.particle.VeUndervoidParticle;
 import rcarmstrong20.vanilla_expansions.client.renderer.particle.VeVoidDripParticle;
 import rcarmstrong20.vanilla_expansions.core.VeParticleTypes;
 
@@ -20,5 +21,6 @@ public class ClientProxy extends CommonProxy
 		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.DRIPPING_VOID, VeVoidDripParticle.DrippingVoidFactory::new);
 		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.FALLING_VOID, VeVoidDripParticle.FallingVoidFactory::new);
 		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.LANDING_VOID, VeVoidDripParticle.LandingVoidFactory::new);
+		Minecraft.getInstance().particles.registerFactory(VeParticleTypes.UNDERVOID, VeUndervoidParticle.Factory::new);
 	}
 }
