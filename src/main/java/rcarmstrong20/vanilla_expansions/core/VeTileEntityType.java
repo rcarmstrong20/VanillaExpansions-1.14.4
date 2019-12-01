3b541889a00dcaa77b850f08f585979007231f7b
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-12-01 00:10:50 -0700

    Created a custom tile entity for my campfires

--- src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/core/VeTileEntityType.java
@@ -3,14 +3,14 @@ package rcarmstrong20.vanilla_expansions.core;
 import java.util.ArrayList;
 import java.util.List;
 
-import net.minecraft.block.Blocks;
-import net.minecraft.tileentity.CampfireTileEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
+import rcarmstrong20.vanilla_expansions.tile_entity.VeCampfireTileEntity;
 
 /**
  * Author: rcarmstrong20
@@ -20,9 +20,9 @@ public class VeTileEntityType
 {
 	public static final List<TileEntityType<?>> TILE_ENTITY_TYPES = new ArrayList<>();
 	
-	public static final TileEntityType<CampfireTileEntity> campfire = buildType("campfire", TileEntityType.Builder.create(CampfireTileEntity::new, Blocks.CAMPFIRE, VeBlocks.white_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
+	public static final TileEntityType<VeCampfireTileEntity> campfire = buildType(VanillaExpansions.location("campfire"), TileEntityType.Builder.create(VeCampfireTileEntity::new, VeBlocks.white_campfire, VeBlocks.orange_campfire, VeBlocks.magenta_campfire, VeBlocks.light_blue_campfire, VeBlocks.yellow_campfire, VeBlocks.lime_campfire, VeBlocks.pink_campfire, VeBlocks.gray_campfire, VeBlocks.light_gray_campfire, VeBlocks.cyan_campfire, VeBlocks.purple_campfire, VeBlocks.blue_campfire, VeBlocks.brown_campfire, VeBlocks.green_campfire, VeBlocks.red_campfire, VeBlocks.black_campfire));
 	
-	private static <T extends TileEntity> TileEntityType<T> buildType(String name, TileEntityType.Builder<T> builder)
+	private static <T extends TileEntity> TileEntityType<T> buildType(ResourceLocation name, TileEntityType.Builder<T> builder)
     {
         TileEntityType<T> type = builder.build(null);
         type.setRegistryName(name);
