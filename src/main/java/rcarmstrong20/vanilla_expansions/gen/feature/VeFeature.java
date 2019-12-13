ec174a76abdae16f3f34590da3d9293d6e76e641
rcarmstrong20 <rcarmstrong20@gmail.com>
2019-12-13 15:05:31 -0700

    Added code for the taiga cabin structure

--- src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/VeFeature.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/VeFeature.java
@@ -9,12 +9,12 @@ import net.minecraft.world.gen.feature.structure.DesertPyramidStructure;
 import net.minecraft.world.gen.feature.structure.Structure;
 import rcarmstrong20.vanilla_expansions.block.VeBerryBushBlock;
 import rcarmstrong20.vanilla_expansions.core.VeBlocks;
-import rcarmstrong20.vanilla_expansions.gen.feature.structure.TaigaCabinFeature;
+import rcarmstrong20.vanilla_expansions.gen.feature.structure.TaigaCabinStructure;
 
 public class VeFeature
 {
 	public static final Feature<BigMushroomFeatureConfig> HUGE_PURPLE_MUSHROOM = new VeBigPurpleMushroomFeature(BigMushroomFeatureConfig::deserialize);
 	public static final Feature<NoFeatureConfig> BLUEBERRY_BUSH = new ScatteredPlantFeature(NoFeatureConfig::deserialize, VeBlocks.blueberry_bush.getDefaultState().with(VeBerryBushBlock.AGE, Integer.valueOf(3)));
 	public static final Feature<NoFeatureConfig> CRANBERRY_BUSH = new ScatteredPlantFeature(NoFeatureConfig::deserialize, VeBlocks.cranberry_bush.getDefaultState().with(VeBerryBushBlock.AGE, Integer.valueOf(3)));
-	public static final Feature<NoFeatureConfig> TAIGA_CABIN = new TaigaCabinFeature(NoFeatureConfig::deserialize);
+	public static final Structure<NoFeatureConfig> TAIGA_CABIN = new TaigaCabinStructure(NoFeatureConfig::deserialize);
 }
--- src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/structure/TaigaCabinStructure.java
+++ src/main/java/rcarmstrong20/vanilla_expansions/gen/feature/structure/TaigaCabinStructure.java
@@ -1,57 +1,108 @@
 package rcarmstrong20.vanilla_expansions.gen.feature.structure;
 
-import java.util.Random;
 import java.util.function.Function;
 
 import com.mojang.datafixers.Dynamic;
 
-import net.minecraft.util.Mirror;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.Rotation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
 import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.world.IWorld;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.ChunkGenerator;
-import net.minecraft.world.gen.GenerationSettings;
-import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
-import net.minecraft.world.gen.feature.template.BlockIgnoreStructureProcessor;
-import net.minecraft.world.gen.feature.template.PlacementSettings;
-import net.minecraft.world.gen.feature.template.Template;
+import net.minecraft.world.gen.feature.structure.ScatteredStructure;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraft.world.gen.feature.template.TemplateManager;
-import net.minecraft.world.server.ServerWorld;
 import rcarmstrong20.vanilla_expansions.VanillaExpansions;
 
-public class TaigaCabinFeature extends Feature<NoFeatureConfig>
+public class TaigaCabinStructure extends ScatteredStructure<NoFeatureConfig> 
 {
-	public static final ResourceLocation STRUCTURE_PATH = VanillaExpansions.location("taiga_cabin");
-	
-	public TaigaCabinFeature(Function<Dynamic<?>, ? extends NoFeatureConfig> p_i49888_1_)
+	public TaigaCabinStructure(Function<Dynamic<?>, ? extends NoFeatureConfig> config)
 	{
-		super(p_i49888_1_);
+		super(config);
 	}
 	
+	/**
+	 * A modifier to the random seed for scattered structures, so that every structure does not spawn in the same place
+	 *
+	 * @return a number, vanilla likes to specifically use 8 digit numbers
+	 */
 	@Override
-	public boolean place(IWorld worldIn, ChunkGenerator<? extends GenerationSettings> generator, Random rand, BlockPos pos, NoFeatureConfig config)
+	protected int getSeedModifier()
 	{
-		Random random = worldIn.getRandom();
-		Rotation[] arotation = Rotation.values();
-		Rotation rotation = arotation[random.nextInt(arotation.length)];
-		TemplateManager templatemanager = ((ServerWorld)worldIn.getWorld()).getSaveHandler().getStructureTemplateManager();
-		Template template = templatemanager.getTemplateDefaulted(STRUCTURE_PATH);
-		ChunkPos chunkpos = new ChunkPos(pos);
-		MutableBoundingBox mutableboundingbox = new MutableBoundingBox(chunkpos.getXStart(), 0, chunkpos.getZStart(), chunkpos.getXEnd(), 256, chunkpos.getZEnd());
-		PlacementSettings placementsettings = (new PlacementSettings()).setRotation(rotation).setBoundingBox(mutableboundingbox).setRandom(random).addProcessor(BlockIgnoreStructureProcessor.AIR_AND_STRUCTURE_BLOCK);
-		BlockPos blockpos = template.transformedSize(rotation);
-		int x = random.nextInt(16 - blockpos.getX());
-		int y = generator.getGroundHeight();
-		int z = random.nextInt(16 - blockpos.getZ());
-		BlockPos blockpos1 = new BlockPos(x, y, z);
+		return 12345678;
+	}
+	
+	/**
+	 * Get the start of the structure
+	 * In vanilla the start is usually a static inner class of the structure class, follow this convention if you wish
+	 *
+	 * @return method reference to Start constructor
+	 */
+	@Override
+	public Structure.IStartFactory getStartFactory()
+	{
+		return TaigaCabinStructure.Start::new;
+	}
+	
+	/**
+	 * An 'id' for the structure, distinct from registry id
+	 * Used for the Locate command (by forge only, vanilla uses its own system)
+	 * Should probably be in the format 'modid:name'
+	 *
+	 * @return name of structure
+	 */
+	@Override
+	public String getStructureName()
+	{
+		return VanillaExpansions.MOD_ID + ":" + "taiga_cabin";
+	}
+	
+	/**
+	 * Legacy code, only used to update from old structure format
+	 *
+	 * @return irrelevant
+	 */
+	@Override
+	public int getSize()
+	{
+		return 3;
+	}
+	
+	/**
+	 * The structure start is responsible for creating the structure in memory, but not for placing the blocks themselves
+	 */
+	public static class Start extends StructureStart
+	{
+		public Start(Structure<?> structure, int chunkX, int chunkZ, Biome biome, MutableBoundingBox boundingBox, int references, long seed)
+		{
+			super(structure, chunkX, chunkZ, biome, boundingBox, references, seed);
+		}
 		
-		BlockPos blockpos2 = template.getZeroPositionWithTransform(blockpos1, Mirror.NONE, rotation);
-		System.out.print(blockpos1);
-		template.addBlocksToWorld(worldIn, blockpos2, placementsettings, 4);
-		return true;
+		/**
+		 * For most structures this is the only method you will need to care about
+		 * Not a lot needs to be done here, most of the work is done by structure pieces
+		 * Examples of things vanilla does for different structures here include:
+		 * - Getting configs from the chunk generator
+		 * - Deciding the rotation of the structure
+		 * - Getting height (rarely, most times height is determined in the piece)
+		 */
+		@Override
+		public void init(ChunkGenerator<?> generator, TemplateManager templateManager, int chunkX, int chunkZ, Biome biome)
+		{
+			ResourceLocation templateResource = VanillaExpansions.location("taiga_cabin");
+			Rotation rotation = Rotation.values()[this.rand.nextInt(Rotation.values().length)];
+			
+			TaigaCabinPiece piece = new TaigaCabinPiece(templateManager, templateResource, new BlockPos(chunkX * 16, 0, chunkZ * 16), rotation);
+			
+			//The important thing is that pieces are added like this
+			//See the shipwreck and igloo for an alternate take on this process
+			this.components.add(piece);
+			
+			//This should be called last, after all components have been added
+			this.recalculateStructureSize();
+		}
 	}
 }
